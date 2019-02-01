6cafbf9e3ebf6467c38eb2eb453c1173b11af709
O1sims <sims.owen@gmail.com>
2019-02-01 14:59:13 +0000

    Extend analysis on HE and UCAS

--- analysis/R/analyse_he_enrolements.R
+++ analysis/R/analyse_he_enrolements.R
@@ -2,15 +2,28 @@ library(readr)
 library(magrittr)
 
 
-ROOT_DIRECTORY <- "~/Code/DigitalStrategyNI/"
+get_enrolments <- function(data, subject, level, mode, region, marker) {
+  enrolment_data <- data %>% 
+    subset(`Level of study` == level) %>%
+    subset(`Subject of study` == subject) %>%
+    subset(`Mode of study` == mode) %>%
+    subset(`Domicile marker` == region) %>%
+    subset(`First year marker` == marker)
+  
+  enrolment_data$`Academic Year` <- sapply(
+    X = strsplit(
+      x = enrolment_data$`Academic Year`, 
+      split = "/"),
+    FUN = `[`, 1)
+  
+  return(enrolment_data)
+}
 
 # he_student_enrolments_by_subject_area_all <- read_csv(
-#   paste0(ROOT_DIRECTORY, "analysis/data/rdata/he-student-enrolments-by-subject-area-all.csv"))
+#   paste0(getwd(), "analysis/data/rdata/he-student-enrolments-by-subject-area-all.csv"))
 
-load(
-  file = paste0(
-    ROOT_DIRECTORY, 
-    "analysis/data/rdata/he_student_enrolments_by_subject_area_all.rda"))
+load(file = getwd() %>% 
+       paste0("/analysis/data/rdata/he_student_enrolments_by_subject_area_all.rda"))
 
 he_student_enrolments_by_subject_area_all$`Subject of study` <- sapply(
   X = strsplit(
@@ -23,20 +36,19 @@ sub_enrolment_data <- get_enrolments(
   subject = "Computer science",
   level = "All undergraduate",
   mode = "All",
+  marker = "First year",
   region = "Northern Ireland")
 
-get_enrolments <- function(data, subject, level, mode, region) {
-  enrolment_data <- data %>% 
-    subset(`Level of study`==level) %>%
-    subset(`Subject of study`==subject) %>%
-    subset(`Mode of study`==mode) %>%
-    subset(`Domicile marker`==region)
-  
-  enrolment_data$`Academic Year` <- sapply(
-    X = strsplit(
-      x = enrolment_data$`Academic Year`, 
-      split = "/"),
-    FUN = `[`, 1)
-  
-  return(enrolment_data)
-}
+he_application_chart <- ggplot(
+  data = sub_enrolment_data,
+  aes(
+    x = `Academic Year`, 
+    y = Number, 
+    group = `Subject of study marker`,
+    colour = `Subject of study marker`)) +
+  geom_point() +
+  geom_line() +
+  theme_minimal() +
+  theme(legend.position = "bottom")
+
+he_application_chart
--- analysis/R/analyse_ucas_offers.R
+++ analysis/R/analyse_ucas_offers.R
@@ -1,27 +1,65 @@
-library(readr)
 library(ggplot2)
+library(magrittr)
 
-
-ROOT_DIRECTORY <- "~/Code/DigitalStrategyNI/"
-
-
-# ucas_hosts <- read_csv(file = paste0(ROOT_DIRECTORY, "data/raw-data/ucas-acceptances-by-country-subject-host.csv"))
-# ucas_applicants <- read_csv(file = paste0(ROOT_DIRECTORY, "data/raw-data/ucas-acceptances-by-country-subject-applicant.csv"))
+# library(readr)
+# ucas_hosts <- read_csv(file = paste0(getwd(), "/data/raw-data/ucas-acceptances-by-country-subject-host.csv"))
+# ucas_applicants <- read_csv(file = paste0(getwd(), "/data/raw-data/ucas-acceptances-by-country-subject-applicant.csv"))
 # save(ucas_hosts,
-#      file = paste0(ROOT_DIRECTORY, "data/R/RData/ucas_hosts.rda"))
+#      file = paste0(getwd(), "/data/R/RData/ucas_hosts.rda"))
 # save(ucas_applicants,
-#      file = paste0(ROOT_DIRECTORY, "data/R/RData/ucas_applicants.rda"))
+#      file = paste0(getwd(), "/data/R/RData/ucas_applicants.rda"))
 
-load(file = paste0(ROOT_DIRECTORY, "analysis/data/rdata/ucas_hosts.rda"))
-load(file = paste0(ROOT_DIRECTORY, "analysis/data/rdata/ucas_applicants.rda"))
+load(file = getwd() %>% paste0("/analysis/data/rdata/ucas_hosts.rda"))
+load(file = getwd() %>% paste0("/analysis/data/rdata/ucas_applicants.rda"))
 
 computer_subjects <- c()
 for (subject in ucas_applicants$`Subject Group (Detailed Level)` %>% unique()) {
-  if (grepl("compu", tolower(subject))) {
+  lowerSubject <- subject %>%
+    tolower()
+  if (grepl("compu", lowerSubject) || grepl("cyber", lowerSubject)) {
     computer_subjects %<>% append(subject)
   }
 }
 
-ucas_applicants_computer <- ucas_applicants %>%
-  subset(`Subject Group (Detailed Level)` == "'I1 - Computer Science'") %>%
-  subset(`Acceptance Route` == "'Firm choice'")
+filters <- list(
+  subject = "'I1 - Computer Science'",
+  applicationRoute = "'Firm choice'",
+  domicile = "'Northern Ireland'")
+
+ucas_data <- ucas_applicants %>%
+  subset(`Subject Group (Detailed Level)` == filters$subject) %>%
+  subset(`Acceptance Route` == filters$applicationRoute) %>%
+  subset(`Applicant Domicile (High Level)` == filters$domicile)
+
+ucas_data$hostEntrants <- ucas_hosts %>%
+  subset(`Subject Group (Detailed Level)` == filters$subject) %>%
+  subset(`Acceptance Route` == filters$applicationRoute) %>%
+  subset(`Provider Country` == filters$domicile) %$% 
+  `Number of Acceptances`
+
+ucas_data$diff <- ucas_data$`Number of Acceptances` - 
+  ucas_hosts_computer$`Number of Acceptances`
+
+ucas_data$vacanciesRecorded <- c(540, 505, 530, 560, 610, 635, 700, 775, 760, 715, 785, 745)
+
+ucas_application_chart <- ggplot(
+    data = ucas_data,
+    aes(x = `Cycle Year`)) + 
+  geom_point(aes(y = `Number of Acceptances`,
+                 colour = "NI Applications")) +
+  geom_line(aes(y = `Number of Acceptances`,
+                colour = "NI Applications")) + 
+  geom_point(aes(y = hostEntrants,
+                 colour = "NI Entrants")) +
+  geom_line(aes(y = hostEntrants, 
+                colour = "NI Entrants")) +
+  theme_minimal()
+
+ucas_gap_chart <- ggplot(
+  data = ucas_data,
+  aes(x = `Cycle Year`)) + 
+  geom_point(aes(y = diff, 
+                 colour = "Applicant / entrant gap")) + 
+  geom_line(aes(y = diff, 
+                colour = "Applicant / entrant gap")) +
+  theme_minimal()
