321151b1576d8fe010dedf34ef9d051792f3c0c4
O1sims <sims.owen@gmail.com>
2019-02-10 11:22:35 +0000

    Update analysis on GCSE/GCE and UCAS offers

--- analysis/R/analyse_gcse_alevels.R
+++ analysis/R/analyse_gcse_alevels.R
@@ -1,11 +1,13 @@
-library(readr)
 library(ggplot2)
 library(ggthemes)
 library(magrittr)
 
 
-gcse_alevels <- read_csv(
-  file = "analysis/data/raw-data/ni-gcse-and-alevels.csv")
+# library(readr)
+# 
+# gcse_alevels <- getwd() %>% 
+#   paste0("/analysis/data/raw-data/ni-gcse-and-alevels.csv") %>%
+#   read_csv()
 
 
 chart_level_totals <- function(full_data, level = c("A Level", "AS Level", "GCSE"), gender = TRUE) {
@@ -47,6 +49,9 @@ chart_level_totals <- function(full_data, level = c("A Level", "AS Level", "GCSE
   names(melted_totals_df) <- c(
     "year", "var", "value")
   
+  maxYear <- melted_totals_df$year %>% max()
+  minYear <- melted_totals_df$year %>% min()
+  
   if (gender) {
     melted_totals_df %<>% 
       subset(var!="totalNumber")
@@ -54,7 +59,7 @@ chart_level_totals <- function(full_data, level = c("A Level", "AS Level", "GCSE
       data = melted_totals_df,
       aes(x = year, y = value)) + 
       geom_area(position = 'stack') +
-      labs(title = paste0("Number of ", level, " students (2012-2018)")) +
+      labs(title = paste0("Number of ", level, " students (", minYear, "-", maxYear, ")")) +
       ylab(label = "Number") + xlab(label = "Year") +
       theme_minimal()
   } else {
@@ -63,21 +68,22 @@ chart_level_totals <- function(full_data, level = c("A Level", "AS Level", "GCSE
     chart <- ggplot(
       data = melted_totals_df,
       aes(x = year, y = value)) + 
-      geom_point() + geom_line() +
-      labs(title = paste0("Number of ", level, " students (2012-2018)")) +
+      geom_point() + 
+      geom_line() +
+      labs(title = paste0("Number of ", level, " students (", minYear, "-", maxYear, ")")) +
       ylab(label = "Number") + xlab(label = "Year") +
       theme_minimal()
   }
   
-  chart <- ggplot(
-    data = melted_totals_df,
-    aes(x = year, y = value)) + 
-    geom_area(position = 'stack') +
-    labs(title = paste0("Number of ", level, " students (2012-2018)")) +
-    ylab(label = "Number") + xlab(label = "Year") +
-    theme_minimal()
-  
   return(chart)
 }
 
+load(file = getwd() %>%
+       paste0("/analysis/data/rdata/gcse_alevels.rda"))
+
+
+chart_level_totals(
+  full_data = gcse_alevels, 
+  level = "AS Level", 
+  gender = FALSE)
 
--- analysis/R/analyse_ucas_offers.R
+++ analysis/R/analyse_ucas_offers.R
@@ -1,9 +1,12 @@
 library(ggplot2)
+library(ggthemes)
 library(magrittr)
 
 # library(readr)
+#
 # ucas_hosts <- read_csv(file = paste0(getwd(), "/data/raw-data/ucas-acceptances-by-country-subject-host.csv"))
 # ucas_applicants <- read_csv(file = paste0(getwd(), "/data/raw-data/ucas-acceptances-by-country-subject-applicant.csv"))
+#
 # save(ucas_hosts,
 #      file = paste0(getwd(), "/data/R/RData/ucas_hosts.rda"))
 # save(ucas_applicants,
@@ -12,30 +15,35 @@ library(magrittr)
 load(file = getwd() %>% paste0("/analysis/data/rdata/ucas_hosts.rda"))
 load(file = getwd() %>% paste0("/analysis/data/rdata/ucas_applicants.rda"))
 
+
 computer_subjects <- c()
 for (subject in ucas_applicants$`Subject Group (Detailed Level)` %>% unique()) {
   lowerSubject <- subject %>%
     tolower()
-  if (grepl("compu", lowerSubject) || grepl("cyber", lowerSubject)) {
-    computer_subjects %<>% append(subject)
+  if ((grepl("compu", lowerSubject) || grepl("cyber", lowerSubject) || 
+      grepl("engineer", lowerSubject) || grepl("software", lowerSubject)) && 
+      (!grepl("comb", lowerSubject) & !grepl("other", lowerSubject) & 
+       !grepl("any", lowerSubject) & !grepl("general", lowerSubject) & 
+       !grepl("audio", lowerSubject))) {
+    computer_subjects %<>% 
+      append(subject)
   }
 }
 
 filters <- list(
-  subject = "'I1 - Computer Science'",
+  subjects = computer_subjects,
   applicationRoute = "'Firm choice'",
   domicile = "'Northern Ireland'")
 
 ucas_data <- ucas_applicants %>%
-  subset(`Subject Group (Detailed Level)` == filters$subject) %>%
+  subset(`Subject Group (Detailed Level)` %in% filters$subject) %>%
   subset(`Acceptance Route` == filters$applicationRoute) %>%
   subset(`Applicant Domicile (High Level)` == filters$domicile)
 
-ucas_data$hostEntrants <- ucas_hosts %>%
-  subset(`Subject Group (Detailed Level)` == filters$subject) %>%
+hostEntrants <- ucas_hosts %>%
+  subset(`Subject Group (Detailed Level)` %in% filters$subject) %>%
   subset(`Acceptance Route` == filters$applicationRoute) %>%
-  subset(`Provider Country` == filters$domicile) %$% 
-  `Number of Acceptances`
+  subset(`Provider Country` == filters$domicile)
 
 ucas_data$diff <- ucas_data$`Number of Acceptances` - 
   ucas_hosts_computer$`Number of Acceptances`
@@ -45,14 +53,19 @@ ucas_data$vacanciesRecorded <- c(540, 505, 530, 560, 610, 635, 700, 775, 760, 71
 ucas_application_chart <- ggplot(
     data = ucas_data,
     aes(x = `Cycle Year`)) + 
-  geom_point(aes(y = `Number of Acceptances`,
-                 colour = "NI Applications")) +
-  geom_line(aes(y = `Number of Acceptances`,
-                colour = "NI Applications")) + 
-  geom_point(aes(y = hostEntrants,
-                 colour = "NI Entrants")) +
-  geom_line(aes(y = hostEntrants, 
-                colour = "NI Entrants")) +
+  geom_area(aes(
+    y = `Number of Acceptances`,
+    fill = `Subject Group (Detailed Level)`)) +
+  labs(title = "UK university acceptances by NI applicants") +
+  theme_minimal()
+
+ucas_acceptance_chart <- ggplot(
+  data = hostEntrants,
+  aes(x = `Cycle Year`)) +
+  geom_area(aes(
+    y = `Number of Acceptances`,
+    fill = `Subject Group (Detailed Level)`)) +
+  labs(title = "NI university acceptances by all applicants") +
   theme_minimal()
 
 ucas_gap_chart <- ggplot(
