1932e8b36cdaaf21477e3ec5a395b201667cebce
O1sims <sims.owen@gmail.com>
2019-02-12 19:36:13 +0000

    Update analysis of UCAS offers

--- analysis/R/analyse_ucas_offers.R
+++ analysis/R/analyse_ucas_offers.R
@@ -24,7 +24,9 @@ for (subject in ucas_applicants$`Subject Group (Detailed Level)` %>% unique()) {
       grepl("engineer", lowerSubject) || grepl("software", lowerSubject)) && 
       (!grepl("comb", lowerSubject) & !grepl("other", lowerSubject) & 
        !grepl("any", lowerSubject) & !grepl("general", lowerSubject) & 
-       !grepl("audio", lowerSubject))) {
+       !grepl("audio", lowerSubject) & !grepl("aerospace", lowerSubject) & 
+       !grepl("civil", lowerSubject) & !grepl("mechanical", lowerSubject) & 
+       !grepl("manufact", lowerSubject) & !grepl("chemical", lowerSubject))) {
     computer_subjects %<>% 
       append(subject)
   }
@@ -45,34 +47,69 @@ hostEntrants <- ucas_hosts %>%
   subset(`Acceptance Route` == filters$applicationRoute) %>%
   subset(`Provider Country` == filters$domicile)
 
-ucas_data$diff <- ucas_data$`Number of Acceptances` - 
-  ucas_hosts_computer$`Number of Acceptances`
+ucas_data$demandSupplyDiff <- ucas_data$`Number of Acceptances` - 
+  hostEntrants$`Number of Acceptances`
 
-ucas_data$vacanciesRecorded <- c(540, 505, 530, 560, 610, 635, 700, 775, 760, 715, 785, 745)
+ucas_data$vacanciesRecorded <- c(
+  540, 505, 530, 560, 610, 635, 700, 775, 760, 715, 785, 745)
 
 ucas_application_chart <- ggplot(
     data = ucas_data,
-    aes(x = `Cycle Year`)) + 
-  geom_area(aes(
-    y = `Number of Acceptances`,
-    fill = `Subject Group (Detailed Level)`)) +
-  labs(title = "UK university acceptances by NI applicants") +
+    aes(x = `Cycle Year`,
+        y = `Number of Acceptances`,
+        fill = `Subject Group (Detailed Level)`)) + 
+  geom_area(
+    colour = "black", 
+    size = .2, 
+    alpha = .4) +
+  labs(title = paste0('UK university acceptances by all ', filters$applicationRoute, ' NI applicants (2007-2018)')) +
+  ylab("Number of acceptances") + xlab("Year") +
+  scale_fill_discrete(
+    name = "Subject",
+    labels = c("Electronic & Electrical Engineering", "Computer Science", "Software Engineering")) +
   theme_minimal()
+
+ggsave(
+  filename = getwd() %>% paste0("/analysis/images/ucas-applications.png"), 
+  plot = ucas_application_chart, 
+  device = "png")
 
 ucas_acceptance_chart <- ggplot(
   data = hostEntrants,
-  aes(x = `Cycle Year`)) +
-  geom_area(aes(
-    y = `Number of Acceptances`,
-    fill = `Subject Group (Detailed Level)`)) +
-  labs(title = "NI university acceptances by all applicants") +
+  aes(x = `Cycle Year`,
+      y = `Number of Acceptances`,
+      fill = `Subject Group (Detailed Level)`)) +
+  geom_area(
+    colour = "black", 
+    size = .2, 
+    alpha = .4) +
+  labs(title = paste0("All NI University acceptances by all ", filters$applicationRoute," applicants (2007--2018)")) +
+  ylab("Number of acceptances") + xlab("Year") +
+  scale_fill_discrete(
+    name = "Subject",
+    labels = c("Electronic & Electrical Engineering", "Computer Science", "Software Engineering")) +
   theme_minimal()
+
+ggsave(
+  filename = getwd() %>% paste0("/analysis/images/ucas-acceptance.png"), 
+  plot = ucas_acceptance_chart, 
+  device = "png")
 
 ucas_gap_chart <- ggplot(
   data = ucas_data,
   aes(x = `Cycle Year`)) + 
-  geom_point(aes(y = diff, 
-                 colour = "Applicant / entrant gap")) + 
-  geom_line(aes(y = diff, 
-                colour = "Applicant / entrant gap")) +
+  geom_point(aes(y = demandSupplyDiff * 2,
+                 colour = `Subject Group (Detailed Level)`)) + 
+  geom_line(aes(y = demandSupplyDiff * 2, 
+                colour = `Subject Group (Detailed Level)`)) +
+  labs(title = paste0("Difference between number of University applications and number of acceptances (2007--2018)")) +
+  ylab("Demand / supply difference") + xlab("Year") +
+  scale_colour_discrete(
+    name = "Subject",
+    labels = c("Electronic & Electrical Engineering", "Computer Science", "Software Engineering")) +
   theme_minimal()
+
+ggsave(
+  filename = getwd() %>% paste0("/analysis/images/ucas-demand-suplus.png"), 
+  plot = ucas_gap_chart, 
+  device = "png")
