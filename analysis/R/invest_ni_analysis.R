ec772af958e02a51db064e3d22ffdbefca1c6f71
O1sims <sims.owen@gmail.com>
2019-03-10 22:27:29 +0000

    Finish up charts from Invest NI analysis

--- analysis/R/invest_ni_analysis.R
+++ analysis/R/invest_ni_analysis.R
@@ -14,7 +14,8 @@ generate_ict_proportion_projects <- function(INIData, SICSector) {
       subset(`Financial Year Offer Made` == year) %$%
       `SIC Sector`
     ictPercentage %<>% 
-      append(values = 100 * (sum(sicSectors == SICSector) / length(sicSectors)))
+      append(values = 100 * 
+               (sum(sicSectors == SICSector) / length(sicSectors)))
   }
   
   percentageChart <- data.frame(
@@ -45,7 +46,7 @@ generate_job_creation_bar <- function(INIData, SICSector = NULL) {
       aes(
         x = `Financial Year Offer Made`, 
         y = `Jobs to be Created (Assisted)`,
-        fill = `SIC Sector`), 
+        fill = `Ownership when the offer was made`), 
       stat = "identity") +
     ylab(label = "Jobs created") +
     xlab(label = "Financial year") +
@@ -66,16 +67,24 @@ violin_chart_total_investment <- function(INIData, SICSectors = c()) {
     geom_violin(
       aes(
         x = `SIC Sector`, 
-        y = `Total Investment (Includes Invest NI Assistance)`)) +
+        y = (`Total Investment (Includes Invest NI Assistance)`)/1000000,
+        fill = `SIC Sector`)) +
+    xlab(label = "SIC sector") +
+    scale_y_continuous(
+      name = "Total investment (£ million)", 
+      labels = scales::comma) +
+    theme_minimal() +
     theme(legend.position = "none")
 }
+
 
 total_investment_over_time <- function(INIData, SICSector) {
   INIData %<>% 
     subset(`SIC Sector` == SICSector) 
   
   totalInvestment <- c()
-  allFinancialYears <- INIData$`Financial Year Offer Made` %>% unique()
+  allFinancialYears <- INIData$`Financial Year Offer Made` %>% 
+    unique()
   for (financialYear in allFinancialYears) {
     total <- INIData %>% 
       subset(`Financial Year Offer Made` == financialYear) %$% 
@@ -103,43 +112,6 @@ total_investment_over_time <- function(INIData, SICSector) {
 }
 
 
-# Load up INI data
-INIData <- getwd() %>%
-  paste0("/analysis/data/raw-data/ini-open-data-upload-09jan19.csv") %>%
-  read_csv()
-
-# Clean up some of the data
-INIData$`SIC Sector` %<>% gsub(
-  pattern = "&", 
-  replacement = "And")
-INIData$`SIC Sector` %<>% gsub(
-  pattern = "Wholesale And Retail Trade; Repair Of Motor Vehicles And Motorcycles", 
-  replacement = "Wholesale And Retail Trade And Repair of Vehicles")
-
-# Generate job creation bar chart
-generate_job_creation_bar(
-  INIData = INIData,
-  SICSector = "Information And Communication")
-
-# Generate chart to map the proportion of ICT projects
-generate_ict_proportion_projects(
-  INIData = INIData, 
-  SICSector = "Information And Communication")
-
-# Generate violin chart 
-violin_chart_total_investment(
-  INIData = INIData, 
-  SICSectors = c("Information And Communication"))
-
-# Generate stacked bar chart for total investment
-total_investment_over_time(
-  INIData = INIData, 
-  SICSector = "Information And Communication")
-
-
-
-
-
 # Proportion of investment provided by Invest NI
 average_amount_ini <- function(sector = NA) {
   financed.INI.projects <- INIData %>% 
@@ -156,3 +128,54 @@ average_amount_ini <- function(sector = NA) {
     mean() %>%
     return()
 }
+
+
+# Load up INI data
+INIData <- getwd() %>%
+  paste0("/analysis/data/raw-data/ini-open-data-upload-09jan19.csv") %>%
+  read_csv(progress = TRUE)
+
+# Clean up some of the data
+INIData$`SIC Sector` %<>% gsub(
+  pattern = "&", 
+  replacement = "And")
+INIData$`SIC Sector` %<>% gsub(
+  pattern = "Wholesale And Retail Trade; Repair Of Motor Vehicles And Motorcycles", 
+  replacement = "Wholesale And Retail Trade And Repair of Vehicles")
+
+# Define the SIC sector to analyse
+SICSector <- "Information And Communication"
+
+# Sector of interest only
+ICData <- INIData %>% 
+  subset(`SIC Sector` == SICSector)
+
+# Generate job creation bar chart
+generate_job_creation_bar(
+  INIData = INIData,
+  SICSector = SICSector)
+
+# Generate chart to map the proportion of ICT projects
+generate_ict_proportion_projects(
+  INIData = INIData, 
+  SICSector = SICSector)
+
+# Generate violin chart 
+violin_chart_total_investment(
+  INIData = INIData,
+  SICSectors = c(SICSector))
+
+# Generate stacked bar chart for total investment
+total_investment_over_time(
+  INIData = INIData, 
+  SICSector = SICSector)
+
+# Total number of jobs created in the `Information and Communication` sector
+ICData$`Jobs to be Created (Assisted)` %>%
+  sum()
+
+# Total number of jobs created from external firms in the `Information and Communication` sector
+ICData %>% 
+  subset(`Ownership when the offer was made` == "External") %$%
+  `Jobs to be Created (Assisted)` %>% 
+  sum()
